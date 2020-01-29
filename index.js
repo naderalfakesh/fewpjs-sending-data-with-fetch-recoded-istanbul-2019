c3e2103539e974154870febf196c81304e8e1a2c
Nadir Alfakesh <nader.alfakesh@gmail.com>
2020-01-29 19:07:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Add your code here
+function submitData(name,email){
+let configObj = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify({
+    name: "nader",
+    email: "nader@nader.com"
+  })
+};
+ 
+fetch("http://localhost:3000/users", configObj);
+}
