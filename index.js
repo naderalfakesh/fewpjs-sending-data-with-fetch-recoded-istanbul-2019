86fa49d518d347fb30faef00cf307637a4e9a0c8
Nadir Alfakesh <nader.alfakesh@gmail.com>
2020-01-29 19:17:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your code here
+function submitData(name,email){
+let configObj = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify({
+    name ,
+    email
+  })
+};
+
+return fetch("http://localhost:3000/users", configObj)
+.then(resp => resp.json())
+.then(data => document.querySelector('span').textContent = data.id)
+.catch(err => document.querySelector('span').textContent = err );
+}
